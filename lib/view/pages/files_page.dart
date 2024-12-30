d5f5ed4049333fd0019fce303714c2f756b4a6ec
AhmadAlrimawi2000 <ahmadrimawix2000@gmail.com>
2024-12-30 14:34:07 +0300

    [Add] upload bottom sheet

--- lib/view/pages/files_page.dart
+++ lib/view/pages/files_page.dart
@@ -1,4 +1,7 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_drive_app/constants/app_colors.dart';
+import 'package:simple_drive_app/constants/utils.dart';
 import 'package:simple_drive_app/widgets/folder_section.dart';
 import 'package:simple_drive_app/widgets/recent_files.dart';
 
@@ -8,21 +11,93 @@ class FilesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.only(
-          left: 20,
-          right: 20,
-          top: 30,
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              RecentFiles(),
-              FolderSection(),
-            ],
+      child: Stack(
+        alignment: AlignmentDirectional.bottomEnd,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 20,
+              right: 20,
+              top: 30,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  RecentFiles(),
+                  FolderSection(),
+                ],
+              ),
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: FloatingActionButton(
+              onPressed: () {
+                showUploadDialog(context);
+              },
+              backgroundColor: AppColors.orange,
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  showUploadDialog(BuildContext context) {
+    return showModalBottomSheet(
+        shape: OutlineInputBorder(borderRadius: BorderRadius.zero),
+        context: context,
+        builder: (context) {
+          return FittedBox(
+            child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Icon(
+                            EvaIcons.folderAdd,
+                            size: 32,
+                            color: Colors.grey.shade400,
+                          ),
+                          const SizedBox(width: 5),
+                          Text(
+                            "Folder",
+                            style: textStyle(18, Colors.black, FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // const SizedBox(width: 30),
+                    InkWell(
+                      onTap: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text("Upload",
+                              style:
+                                  textStyle(18, Colors.black, FontWeight.bold)),
+                          const SizedBox(width: 5),
+                          Icon(
+                            EvaIcons.upload,
+                            size: 32,
+                            color: Colors.grey.shade400,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+          );
+        });
   }
 }
